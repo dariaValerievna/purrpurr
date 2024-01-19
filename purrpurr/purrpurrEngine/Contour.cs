2f9e17d01af47cc41d2d1e4c11580da1d28c6d3f
Дарья Хренова <dahahrenova@gmail.com>
2024-01-19 13:43:20 +0500

    first

---
+++ purrpurr/purrpurrEngine/Contour.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace purrpurrPlugin
+{
+    public class ContourFlat
+    {
+        public IGeometricShape GeometricShape { get; }
+        public Side SideWithDoor { get; set; }
+        public Side SideWithWindow { get; set; }
+        public string Name { get; }
+
+        public ContourFlat(IGeometricShape geometricShape, Side sideWithDoor, Side sideWithWindow, string name = "")
+        {
+            GeometricShape = geometricShape;
+            SideWithDoor = sideWithDoor;
+            SideWithWindow = sideWithWindow;
+            Name = name;
+        }
+
+        public override string ToString() => Name;
+    }
+
+}
